46c0901862294f4155141243f538adf0f39b543e
31persson <jperscas@myuax.com>
2023-11-30 12:17:54 +0100

    ej 2

--- ej 2.cpp
+++ ej 2.cpp
@@ -3,13 +3,29 @@
 #include <stack>
 
 int main() {
-    std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
-    std::stack<float, std::list<float>> pila(lista);
+    // Crear una lista para almacenar una secuencia de números decimales
+    std::list<double> miLista = {1.1, 2.2, 3.3, 4.4, 5.5};
 
-    while(!pila.empty()) {
-        std::cout << pila.top() << std::endl;
-        pila.pop();
+    // Crear un adaptador de secuencia stack para la lista
+    std::stack<double, std::list<double>> miPila(miLista);
+
+    // Mostrar los elementos originales de la lista
+    std::cout << "Elementos originales de la lista:" << std::endl;
+    for (const auto& elemento : miLista) {
+        std::cout << elemento << " ";
     }
+    std::cout << std::endl;
+
+    // Operación push en la pila
+    miPila.push(6.6);
+
+    // Mostrar los elementos después de la operación push
+    std::cout << "Elementos de la pila despues de push:" << std::endl;
+    while (!miPila.empty()) {
+        std::cout << miPila.top() << " ";
+        miPila.pop();
+    }
+    std::cout << std::endl;
 
     return 0;
 }
