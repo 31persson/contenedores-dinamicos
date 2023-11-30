0ffce84a6579b12e413c42f11e93c44e80131675
31persson <jperscas@myuax.com>
2023-11-30 12:13:45 +0100

    ej 1

--- ej 1.cpp
+++ ej 1.cpp
@@ -2,19 +2,10 @@
 #include <vector>
 
 int main() {
-    // Crear un vector de enteros
-    std::vector<int> miVector;
+    std::vector<int> numeros = {10, 20, 30, 40, 50};
 
-    // Añadir elementos al vector mediante push_back
-    miVector.push_back(10);
-    miVector.push_back(20);
-    miVector.push_back(30);
-    miVector.push_back(40);
-
-    // Utilizar un iterador para recorrer y mostrar cada elemento del vector
-    std::cout << "Elementos del vector: ";
-    for (std::vector<int>::iterator it = miVector.begin(); it != miVector.end(); ++it) {
-        std::cout << *it << " ";
+    for(std::vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        std::cout << *it << std::endl;
     }
 
     return 0;
